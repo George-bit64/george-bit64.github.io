2b54512e5e6c1251aebca93c4c6223e9ac430321
Jorge Vizcaíno <vizcainojorge57@gmail.com>
2025-06-15 18:20:50 -0500

    Add files via upload
    Initial Commit

---
+++ dinamismo.js
@@ -0,0 +1,8 @@
+function NewImage(nuevaFoto) {
+    document.getElementById('imagendefault').src = nuevaFoto;
+  }
+  
+  function defaultImage() {
+    document.getElementById('imagendefault').src = 'Images/default_image.jpeg';
+  }
+  
