d62cc538dbbb0f4770f6d7a78337807cc63d2f99
Jorge Vizcaíno <vizcainojorge57@gmail.com>
2025-06-15 19:21:34 -0500

    Delete dinamismo.js

--- dinamismo.js
+++
@@ -1,8 +0,0 @@
-function NewImage(nuevaFoto) {
-    document.getElementById('imagendefault').src = nuevaFoto;
-  }
-  
-  function defaultImage() {
-    document.getElementById('imagendefault').src = 'Images/default_image.jpeg';
-  }
-  
